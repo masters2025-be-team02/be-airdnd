40143fca5db510d4263be7e5af9bd68507d6d471
subin <eltm521@naver.com>
2025-06-11 17:02:22 +0900

    chore : JpaAuditingConfig 작성

---
+++ src/main/java/kr/kro/airbob/config/JpaAuditingConfig.java
@@ -0,0 +1,11 @@
+package kr.kro.airbob.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
