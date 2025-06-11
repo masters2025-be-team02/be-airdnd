c72f636111bdf61cc561d6031e223ef7c3fb5956
subin <eltm521@naver.com>
2025-06-11 20:21:12 +0900

    chore : Spring restdocs 설정 추가

--- src/test/java/kr/kro/airbob/domain/accommodation/AccommodationControllerTest.java
+++ src/test/java/kr/kro/airbob/domain/accommodation/AccommodationControllerTest.java
@@ -6,29 +6,32 @@ import static com.fasterxml.jackson.databind.node.JsonNodeType.STRING;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
-import kr.kro.airbob.domain.accommodation.common.AccommodationType;
-import kr.kro.airbob.domain.accommodation.dto.AccommodationRequest.AddressInfo;
-import kr.kro.airbob.domain.accommodation.dto.AccommodationRequest.AmenityInfo;
-import kr.kro.airbob.domain.accommodation.dto.AccommodationRequest.CreateAccommodationDto;
-import kr.kro.airbob.domain.accommodation.dto.AccommodationRequest.OccupancyPolicyInfo;
-import kr.kro.airbob.domain.accommodation.entity.Accommodation;
+import kr.kro.airbob.domain.accommodation.dto.AccommodationRequest.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationExtension;
 
+@ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
 @WebMvcTest(AccommodationController.class)
 class AccommodationControllerTest {
@@ -38,6 +41,35 @@ class AccommodationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+    private RestDocumentationResultHandler document;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.document = document(
+                "{class-name}/{method-name}",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())
+        );
+
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint())
+                        .and()
+                        .uris()
+                        .withScheme("https")
+                        .withHost("api.airbob.kro.kr")
+                        .withPort(443))
+                .build();
+    }
 
     @Test
     @DisplayName("정보를 입력받아서 숙소를 등록한다")
@@ -59,14 +91,9 @@ class AccommodationControllerTest {
                         .street("테헤란로")
                         .build())
                 .amenityInfos(List.of(
-                        AmenityInfo.builder()
-                                .name("bed")
-                                .count(2)
-                                .build(),
-                        AmenityInfo.builder()
-                                .name("parking")
-                                .count(1)
-                                .build()))
+                        AmenityInfo.builder().name("bed").count(2).build(),
+                        AmenityInfo.builder().name("parking").count(1).build()
+                ))
                 .occupancyPolicyInfo(OccupancyPolicyInfo.builder()
                         .maxOccupancy(6)
                         .adultOccupancy(4)
@@ -82,7 +109,7 @@ class AccommodationControllerTest {
         // when & then
         mockMvc.perform(post("/api/accommodations")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(request)))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andDo(document("register-accommodation",
                         requestFields(
@@ -93,7 +120,6 @@ class AccommodationControllerTest {
                                 fieldWithPath("thumbnail_url").type(STRING).description("대표 썸네일 URL"),
                                 fieldWithPath("type").type(STRING).description("숙소 타입 (예: HOUSE, APARTMENT 등)"),
 
-                                // addressInfo
                                 fieldWithPath("addressInfo.postalCode").type(NUMBER).description("우편번호"),
                                 fieldWithPath("addressInfo.city").type(STRING).description("도시"),
                                 fieldWithPath("addressInfo.country").type(STRING).description("국가"),
@@ -101,12 +127,10 @@ class AccommodationControllerTest {
                                 fieldWithPath("addressInfo.district").type(STRING).description("구/군"),
                                 fieldWithPath("addressInfo.street").type(STRING).description("도로명 주소"),
 
-                                // amenityInfos
                                 fieldWithPath("amenityInfos").type(ARRAY).description("어메니티 리스트"),
                                 fieldWithPath("amenityInfos[].name").type(STRING).description("어메니티 이름"),
                                 fieldWithPath("amenityInfos[].count").type(NUMBER).description("어메니티 수량"),
 
-                                // occupancyPolicyInfo
                                 fieldWithPath("occupancyPolicyInfo.maxOccupancy").type(NUMBER).description("최대 수용 인원"),
                                 fieldWithPath("occupancyPolicyInfo.adultOccupancy").type(NUMBER).description("성인 인원"),
                                 fieldWithPath("occupancyPolicyInfo.childOccupancy").type(NUMBER).description("아동 인원"),
