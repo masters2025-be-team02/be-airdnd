cb932e2272fc24eca3d020d56317a50a6602bd76
jaehoonchoi <eeoos8840@gmail.com>
2025-06-11 17:00:48 +0900

    feat: WishlistController 생성 및 위시리스트 생성 Controller 로직 구현

---
+++ src/main/java/kr/kro/airbob/domain/wishlist/api/WishlistController.java
@@ -0,0 +1,29 @@
+package kr.kro.airbob.domain.wishlist.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class WishlistController {
+
+	private final WishlistService wishlistService;
+
+	@PostMapping("/members/wishlists")
+	public ResponseEntity<WishlistDto.createResponse> createWishlist(
+		@Valid @RequestBody WishlistDto.createRequest request) {
+		log.info(request.toString());
+		WishlistDto.createResponse response = wishlistService.createWishlist(request);
+		log.info(response.toString());
+		return ResponseEntity.ok(response);
+	}
+}
