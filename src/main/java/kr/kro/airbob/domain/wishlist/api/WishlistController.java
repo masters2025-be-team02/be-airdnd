816a2642d33431996a1b71798e1e425227d9ace8
jaehoonchoi <eeoos8840@gmail.com>
2025-06-11 17:08:41 +0900

    feat: 위시리스트 생성에 대한 요청, 응답 DTO 생성

--- src/main/java/kr/kro/airbob/domain/wishlist/api/WishlistController.java
+++ src/main/java/kr/kro/airbob/domain/wishlist/api/WishlistController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import kr.kro.airbob.domain.wishlist.dto.WishlistDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
---
+++ src/main/java/kr/kro/airbob/domain/wishlist/dto/WishlistDto.java
@@ -0,0 +1,28 @@
+package kr.kro.airbob.domain.wishlist.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class WishlistDto {
+
+	private WishlistDto() {
+	}
+
+	/**
+	 * 요청 DTO
+	 */
+	public record createRequest(
+		@NotBlank(message = "위시리스트 이름은 공백일 수 없습니다.")
+		@Size(max = 255)
+		String name
+	) {
+	}
+
+	/**
+	 * 응답 DTO
+	 */
+	public record createResponse(
+		long id
+	) {
+	}
+}
