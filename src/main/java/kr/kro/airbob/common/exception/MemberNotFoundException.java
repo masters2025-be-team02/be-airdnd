b72c211796f0fdd05fb3fa1c9f27334be1232763
jaehoonchoi <eeoos8840@gmail.com>
2025-06-11 17:27:14 +0900

    feat: 존재하지 않는 사용자 조회 커스텀 에러 생성

--- src/main/java/kr/kro/airbob/common/exception/MemberNotFoundException.java
+++ src/main/java/kr/kro/airbob/common/exception/MemberNotFoundException.java
@@ -1,2 +1,8 @@
-package kr.kro.airbob.common.exception;public class MemberNotFoundException {
+package kr.kro.airbob.common.exception;
+
+public class MemberNotFoundException extends RuntimeException{
+
+	public MemberNotFoundException() {
+		super("존재하지 않는 사용자입니다.");
+	}
 }
--- src/main/java/kr/kro/airbob/domain/wishlist/api/WishlistController.java
+++ src/main/java/kr/kro/airbob/domain/wishlist/api/WishlistController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import kr.kro.airbob.domain.wishlist.WishlistService;
 import kr.kro.airbob.domain.wishlist.dto.WishlistDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +19,13 @@ import lombok.extern.slf4j.Slf4j;
 public class WishlistController {
 
 	private final WishlistService wishlistService;
+	private static final Long TEMP_MEMBER_ID = 1L;
 
 	@PostMapping("/members/wishlists")
 	public ResponseEntity<WishlistDto.createResponse> createWishlist(
 		@Valid @RequestBody WishlistDto.createRequest request) {
 		log.info(request.toString());
-		WishlistDto.createResponse response = wishlistService.createWishlist(request);
+		WishlistDto.createResponse response = wishlistService.createWishlist(request, TEMP_MEMBER_ID);
 		log.info(response.toString());
 		return ResponseEntity.ok(response);
 	}
