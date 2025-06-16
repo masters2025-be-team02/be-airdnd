ac3df0b85cbd3bba2b4ca07a893d4cf4a8a7b9f9
subin <eltm521@naver.com>
2025-06-16 20:49:03 +0900

    feat : 로그인 로그아웃 기능 구혐 - 인증을 위한 필터 추가 - 세션정보를 저장하기 위한 redis 설정 추가

--- src/main/java/kr/kro/airbob/domain/member/MemberRepository.java
+++ src/main/java/kr/kro/airbob/domain/member/MemberRepository.java
@@ -1,7 +1,10 @@
 package kr.kro.airbob.domain.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
