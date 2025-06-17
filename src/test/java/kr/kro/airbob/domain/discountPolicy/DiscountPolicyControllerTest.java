72acc1334034c4212f94a8b42695abed1e531ef4
guzzang <991012kim@gmail.com>
2025-06-17 21:51:01 +0900

    fix: conflict 해결

--- src/test/java/kr/kro/airbob/domain/discountPolicy/DiscountPolicyControllerTest.java
+++ src/test/java/kr/kro/airbob/domain/discountPolicy/DiscountPolicyControllerTest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import kr.kro.airbob.cursor.util.CursorDecoder;
 import kr.kro.airbob.domain.accommodation.interceptor.AccommodationAuthorizationInterceptor;
-import kr.kro.airbob.domain.discountPolicy.DiscountPolicy;
+import kr.kro.airbob.domain.discountPolicy.entity.DiscountPolicy;
 import kr.kro.airbob.domain.discountPolicy.common.DiscountType;
 import kr.kro.airbob.domain.discountPolicy.common.PromotionType;
 import kr.kro.airbob.domain.discountPolicy.dto.request.DiscountPolicyCreateDto;
