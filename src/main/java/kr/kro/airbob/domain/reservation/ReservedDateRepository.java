b22652c80c107492a1172d845f609dd85571d6ba
guzzang <991012kim@gmail.com>
2025-06-17 14:57:51 +0900

    delete: 클래스 제거

--- src/main/java/kr/kro/airbob/domain/reservation/ReservedDateRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.kro.airbob.domain.reservation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservedDateRepository extends JpaRepository<ReservedDate, Long> {
-}
