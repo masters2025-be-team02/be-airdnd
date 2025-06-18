dcd52b01daff5045ce9474cb81fdff74d82e69f5
jaehoonchoi <eeoos8840@gmail.com>
2025-06-18 20:40:59 +0900

    feat: 리뷰 요청에 대한 인터셉터 생성
    - 인터셉터에서 검증 로직 구현
    - 검증을 위한 repository 메서드 구현

--- src/main/java/kr/kro/airbob/domain/review/ReviewRepository.java
+++ src/main/java/kr/kro/airbob/domain/review/ReviewRepository.java
@@ -1,7 +1,11 @@
 package kr.kro.airbob.domain.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+	@Query("select r.accommodation.id from Review r where r.id = :reviewId")
+	Long findAccommodationIdByReviewId(@Param("reviewId") Long reviewId);
 }
