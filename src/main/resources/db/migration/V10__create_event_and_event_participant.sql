d6d68f4f37d53b8d632c485c67eda8f0ce7ae61b
subin <eltm521@naver.com>
2025-06-24 17:34:42 +0900

    fix : DDL 문법오류 수정

--- src/main/resources/db/migration/V10__create_event_and_event_participant.sql
+++ src/main/resources/db/migration/V10__create_event_and_event_participant.sql
@@ -5,7 +5,7 @@ CREATE TABLE event (
                        start_at TIMESTAMP NOT NULL,
                        end_at TIMESTAMP NOT NULL,
                        status VARCHAR(50) NOT NULL,
-                       created_at DATETIME NOT NULL,
+                       created_at DATETIME NOT NULL
 );
 
 CREATE TABLE event_participant (
@@ -17,12 +17,12 @@ CREATE TABLE event_participant (
                                        FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
 );
 
-INSERT INTO event (name, max_participants, start_at, end_at, status, created_at, updated_at)
+INSERT INTO event (name, max_participants, start_at, end_at, status, created_at)
 VALUES (
            '선착순 10000명 이벤트',
            10000,
            '2025-07-01 00:00:00',
            '2025-07-10 23:59:59',
            'OPEN',
-           NOW(),
+           NOW()
        );
