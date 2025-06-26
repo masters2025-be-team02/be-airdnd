5ddc9bbb94388e9d6deaf3218bd1e895596cce60
jaehoonchoi <eeoos8840@gmail.com>
2025-06-26 14:26:06 +0900

    feat: AccommodationSearchRepository 생성

--- src/main/java/kr/kro/airbob/search/repository/AccommodationSearchRepository.java
+++ src/main/java/kr/kro/airbob/search/repository/AccommodationSearchRepository.java
@@ -1,2 +1,9 @@
-package kr.kro.airbob.search.repository;public interface AccommodationSearchRepository {
+package kr.kro.airbob.search.repository;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import kr.kro.airbob.search.document.AccommodationDocument;
+
+public interface AccommodationSearchRepository extends ElasticsearchRepository<AccommodationDocument, Long> {
+
 }
