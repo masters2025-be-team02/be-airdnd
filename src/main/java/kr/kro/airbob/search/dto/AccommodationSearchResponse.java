d89b4a9f5b056c7f5b8a975ec27562788844a67a
jaehoonchoi <eeoos8840@gmail.com>
2025-06-28 15:16:14 +0900

    feat: 리뷰 검색에 대한 요청/응답 dto 생성

--- src/main/java/kr/kro/airbob/search/dto/AccommodationSearchResponse.java
+++ src/main/java/kr/kro/airbob/search/dto/AccommodationSearchResponse.java
@@ -1,2 +1,43 @@
-package kr.kro.airbob.search.dto;public class AccommodationSearchResponse {
+package kr.kro.airbob.search.dto;
+
+import java.util.List;
+
+import kr.kro.airbob.domain.review.dto.ReviewResponse;
+import kr.kro.airbob.geo.dto.Coordinate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AccommodationSearchResponse {
+
+	@Builder
+	public record AccommodationSearchInfo(
+		long id,
+		String name,
+		String locationSummary, // ex) 동작구 사당동
+		List<String> accommodationImageUrls,
+		Coordinate coordinate,
+		PriceResponse pricePerNight,
+		ReviewResponse.ReviewSummary review,
+		String hostName,
+		Boolean isInWishlist
+	){
+	}
+
+	@Builder
+	public record AccommodationSearchInfos(
+		List<AccommodationSearchInfo> StaySearchResultListing,
+		int totalCount
+	){
+	}
+
+
+	@Builder
+	public record  PriceResponse(
+		String currencyCode,
+		String displayPrice,
+		int price
+	){
+	}
 }
